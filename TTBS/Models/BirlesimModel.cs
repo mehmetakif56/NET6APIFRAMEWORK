25c0757b8459d6d7a7948a1e0abb48095c9118bc
mehmetakif.ayd <mehmetakif.aydin@tubitak.gov.tr>
2022-05-13 02:22:01 +0300

    birlesiımKomId

--- TTBS/Models/BirlesimModel.cs
+++ TTBS/Models/BirlesimModel.cs
@@ -4,6 +4,7 @@ namespace TTBS.Models
 {
     public class BirlesimModel
     {
+        public Guid Id { get; set; }
         public string BirlesimNo { get; set; }
         public DateTime? BaslangicTarihi { get; set; }
         public DateTime? BitisTarihi { get; set; }
--- TTBS/Models/KomisyonModel.cs
+++ TTBS/Models/KomisyonModel.cs
@@ -4,6 +4,7 @@ namespace TTBS.Models
 {
     public class KomisyonModel
     {
+        public Guid Id { get; set; }
         public string Ad { get; set; }
         public string Kodu { get; set; }
         public DateTime? Tarih { get; set; }
