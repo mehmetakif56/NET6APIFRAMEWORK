a5a4bfefd95312c581968073a2736fb8d78fb9e5
mehmetakif.ayd <mehmetakif.aydin@tubitak.gov.tr>
2022-05-17 12:56:58 +0300

    IzınModel

--- TTBS/Models/StenoIzinModel.cs
+++ TTBS/Models/StenoIzinModel.cs
@@ -5,6 +5,7 @@ namespace TTBS.Models
 {
     public class StenoIzinModel
     {
+        public Guid Id { get; set; }
         public DateTime? BaslangicTarihi { get; set; }
         public DateTime? BitisTarihi { get; set; }
         //public DateTime? BaslangicSaati { get; set; }
