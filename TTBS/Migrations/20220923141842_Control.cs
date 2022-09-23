57db45dfb7258a25221b9833f44a68da6c087950
hakan.dalkic <hakan.dalkic@tubitak.gov.tr>
2022-09-23 17:30:58 +0300

    re-migrate

---
+++ TTBS/Migrations/20220923141842_Control.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TTBS.Migrations
+{
+    public partial class Control : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
